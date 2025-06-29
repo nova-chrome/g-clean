3ea82c0f212f2791544ebbc7e2fa8ee0a098c6c1
Victor Avila <victoravilamorales95@gmail.com>
2025-06-28 23:23:30 -0700

    chore(config): ✨ update Next.js configuration to disable development indicators

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false, // Disable Next.js development indicators
 };
 
 export default nextConfig;
