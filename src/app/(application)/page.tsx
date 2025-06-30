55a5982f08bbbfce9706c76f632812d7d73a207c
Victor Avila <victoravilamorales95@gmail.com>
2025-06-29 21:32:31 -0700

    feat(page): ✨ implement `DataTable` for displaying messages

--- src/app/(application)/page.tsx
+++ src/app/(application)/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+import { DataTable } from "~/components/ui/data-table";
+import { columns } from "~/features/messages/components/messages-table/columns";
 import { useTRPC } from "~/lib/client/trpc/client";
 
 export default function Home() {
@@ -9,5 +11,9 @@ export default function Home() {
     trpc.messages.getMyMessages.queryOptions()
   );
 
-  return <pre>{JSON.stringify(getMyMessagesQuery.data, null, 2)}</pre>;
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={getMyMessagesQuery.data?.data || []} />
+    </div>
+  );
 }
