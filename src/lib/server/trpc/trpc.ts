086a9e90fbbe922e9cb3257a348de4f45d81a406
Victor Avila <victoravilamorales95@gmail.com>
2025-06-29 20:15:06 -0700

    feat(trpc): ✨ enhance `createTRPCContext` to include Gmail API integration

--- src/lib/server/trpc/trpc.ts
+++ src/lib/server/trpc/trpc.ts
@@ -1,13 +1,29 @@
+import clerk from "@clerk/clerk-sdk-node";
 import { auth } from "@clerk/nextjs/server";
 import { initTRPC, TRPCError } from "@trpc/server";
+import { google } from "googleapis";
 import { cache } from "react";
 import superjson from "superjson";
 
 export type TrpcContext = Awaited<ReturnType<typeof createTRPCContext>>;
 
 export const createTRPCContext = cache(async () => {
+  const clerkAuth = await auth();
+  const [OauthAccessToken] = await clerk.users.getUserOauthAccessToken(
+    clerkAuth.userId || "",
+    "oauth_google"
+  );
+
+  const { token } = OauthAccessToken;
+
+  const gmail = google.gmail({
+    version: "v1",
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
   return {
-    auth: await auth(),
+    auth: clerkAuth,
+    gmail,
   };
 });
 
