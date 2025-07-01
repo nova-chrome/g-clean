3b2a9f710f186b924e867181eccde283482b3dcd
Victor Avila <victoravilamorales95@gmail.com>
2025-06-30 23:40:41 -0700

    fix(layout): 🐛 add `bg-sidebar` class to body for improved styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
       <TRPCReactProvider>
         <html lang="en" suppressHydrationWarning>
           <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            className={`${geistSans.variable} ${geistMono.variable} antialiased bg-sidebar`}
             suppressHydrationWarning
           >
             <ThemeProvider
