fa2122f6f60b5092c520826f656d5de27169473a
Victor Avila <victoravilamorales95@gmail.com>
2025-06-30 23:30:54 -0700

    feat(profile): ✨ enhance `UserProfile` component with theme support

--- src/app/(application)/profile/[[...user-profile]]/page.tsx
+++ src/app/(application)/profile/[[...user-profile]]/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import { UserProfile } from "@clerk/nextjs";
+import { dark, experimental__simple as simple } from "@clerk/themes";
+import { useTheme } from "next-themes";
 
 export default function AccountPage() {
+  const { theme } = useTheme();
   return (
     <div className="flex justify-center py-10 w-full">
-      <UserProfile />
+      <UserProfile
+        appearance={{ baseTheme: theme === "dark" ? dark : simple }}
+      />
     </div>
   );
 }
