5db7fbfec252470440a9cef3f3d1d7022f1505f1
Victor Avila <victoravilamorales95@gmail.com>
2025-07-26 13:17:22 -0700

    feat(layout): ✨ wrap application layout with `Suspense` for improved loading state handling

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Suspense } from "react";
 import { ScrollArea, ScrollBar } from "~/components/ui/scroll-area";
 import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
 import { AppHeader } from "~/features/layout/components/app-header";
@@ -10,17 +10,19 @@ export default function ApplicationLayout({
   children,
 }: Readonly<PropsWithChildren>) {
   return (
-    <SidebarProvider>
-      <div className="flex h-full w-full">
-        <AppSidebar />
-        <SidebarInset className="flex-1 overflow-hidden">
-          <AppHeader />
-          <ScrollArea className="h-[calc(100vh-5rem)] px-4" type="auto">
-            {children}
-            <ScrollBar orientation="horizontal" />
-          </ScrollArea>
-        </SidebarInset>
-      </div>
-    </SidebarProvider>
+    <Suspense>
+      <SidebarProvider>
+        <div className="flex h-full w-full">
+          <AppSidebar />
+          <SidebarInset className="flex-1 overflow-hidden">
+            <AppHeader />
+            <ScrollArea className="h-[calc(100vh-5rem)] px-4" type="auto">
+              {children}
+              <ScrollBar orientation="horizontal" />
+            </ScrollArea>
+          </SidebarInset>
+        </div>
+      </SidebarProvider>
+    </Suspense>
   );
 }
